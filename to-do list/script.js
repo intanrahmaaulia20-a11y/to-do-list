9e96c0a79521b78f45513e4839b7626504a2e342
Intan Rahma Aulia <intanrahmaaulia20@gmail.com>
2026-09-17 14:39:45 +0700

    Add local storage

--- to-do list/script.js
+++ to-do list/script.js
@@ -1,39 +1,75 @@
+// Menampilkan tugas yang sudah tersimpan
+window.onload = function () {
+    loadTasks();
+};
+
+
+// Fungsi menambahkan tugas
 function addTask() {
 
-    // Mengambil isi input
     let input = document.getElementById("taskInput");
-
-    // Mengambil teks yang ditulis
     let task = input.value;
 
-    // Jika input kosong
     if (task === "") {
         alert("Tulis tugas terlebih dahulu!");
         return;
     }
 
-    // Membuat elemen <li>
-    let li = document.createElement("li");
+    // Ambil tugas yang sudah ada
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-    // Memasukkan teks tugas
-    li.innerHTML = `
-        <span>${task}</span>
-        <button class="delete" onclick="deleteTask(this)">
-            Hapus
-        </button>
-    `;
+    // Tambahkan tugas baru
+    tasks.push(task);
 
-    // Memasukkan tugas ke daftar
-    document.getElementById("taskList").appendChild(li);
+    // Simpan ke browser
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 
-    // Mengosongkan input
+    // Kosongkan input
     input.value = "";
+
+    // Tampilkan ulang tugas
+    loadTasks();
 }
 
 
-function deleteTask(button) {
+// Fungsi menampilkan tugas
+function loadTasks() {
 
-    // Menghapus tugas
-    button.parentElement.remove();
+    let taskList = document.getElementById("taskList");
 
+    // Bersihkan daftar
+    taskList.innerHTML = "";
+
+    // Ambil data dari browser
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    tasks.forEach(function (task, index) {
+
+        let li = document.createElement("li");
+
+        li.innerHTML = `
+            <span>${task}</span>
+            <button class="delete" onclick="deleteTask(${index})">
+                Hapus
+            </button>
+        `;
+
+        taskList.appendChild(li);
+    });
+}
+
+
+// Fungsi menghapus tugas
+function deleteTask(index) {
+
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    // Hapus tugas
+    tasks.splice(index, 1);
+
+    // Simpan perubahan
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    // Tampilkan ulang
+    loadTasks();
 }
